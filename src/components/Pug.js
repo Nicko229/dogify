413aca6200ede6a9383ece1657efa415001efc87
Nicko229 <you@example.com>
2019-10-11 20:33:41 +0200

    unstyled pug page functional

--- src/components/Pug.js
+++ src/components/Pug.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import placeholder from './placeholder.jpg';
+import axios from 'axios';
 import Header from './Header'
 
 export default function Pug() {
+  const [pug, setPug] = useState('')
+
+  let handleSubmit = (e) => {
+    e.preventDefault()
+    axios.get(`https://dog.ceo/api/breed/pug/images/random`)
+      .then(res => {
+        console.log(res.data.message);
+        setPug(res.data.message);
+        console.log("pug", pug)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   return (
     <div>
       {/* <Header /> */}
-      <h1>Pug page</h1>
+      <h1>Pug Information Page</h1>
+
+      <input onClick={handleSubmit} className="submit-input" type="submit" value="Submit" />
+
+      <div className="image-div">
+        <img className="dog-image" src={
+          pug === '' ? placeholder : pug
+        } />
+      </div>
     </div>
   )
 };
