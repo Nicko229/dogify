20c9399647f7d659ca53e6ce6d083d74c1c84c36
Nicko229 <you@example.com>
2019-10-22 18:54:24 +0200

    functinal component implemented

--- src/components/auth/Register1.js
+++ src/components/auth/Register1.js
@@ -15,29 +15,30 @@ import {
 import './Register1.css';
 
 
-class Register extends Component {
+function Register(props) {
 
-  clearErrorState = () => {
-    this.props.registerResetErrorsState()
+  let clearErrorState = () => {
+    props.registerResetErrorsState()
   }
 
-  handleSubmit = async event => {
+  let handleSubmit = async event => {
     event.preventDefault();
 
     // Form validation
-    this.clearErrorState();
-    const error = Validate(event, this.props);
+    clearErrorState();
+    const error = Validate(event, props);
 
     if (error) {
-      this.props.registerErrorsState(this.props.errors, error)
+      props.registerErrorsState(props.errors, error)
     }
 
     // AWS Cognito integration here
 
-    const { username, email, password } = this.props;
+    const { username, email, password } = props;
 
     // PROBLEM: Try is resetting props to undefined
     // Can use a promise or not use try catch in async function
+    // email is set to undefined somehow by Auth.signUp
 
     try {
       const signUpResponse = await Auth.signUp({
@@ -52,112 +53,111 @@ class Register extends Component {
     catch (error) {
       let err = null;
       !error.message ? err = { "Message": error } : err = error;
-      this.props.registerCognitoErrorsState(this.props.errors, error)
+      props.registerCognitoErrorsState(props.errors, error)
     }
-    console.log("this.props handleSubmit", this.props)
-    this.props.history.push('/login');
+    console.log("this.props handleSubmit", props)
+    props.history.push('/login');
   };
 
-  onInputUsernameChange = (event) => {
-    this.props.registerUsernameState(event)
+  let onInputUsernameChange = (event) => {
+    props.registerUsernameState(event)
     document.getElementById(event.target.id).classList.remove("is-danger");
   }
 
-  onInputEmailChange = (event) => {
-    this.props.registerEmailState(event)
+  let onInputEmailChange = (event) => {
+    props.registerEmailState(event)
     document.getElementById(event.target.id).classList.remove("is-danger");
   }
 
-  onInputPasswordChange = (event) => {
-    this.props.registerPasswordState(event)
+  let onInputPasswordChange = (event) => {
+    props.registerPasswordState(event)
     document.getElementById(event.target.id).classList.remove("is-danger");
   }
 
-  onInputConfirmPasswordChange = (event) => {
-    this.props.registerConfirmPasswordState(event)
+  let onInputConfirmPasswordChange = (event) => {
+    props.registerConfirmPasswordState(event)
     document.getElementById(event.target.id).classList.remove("is-danger");
   }
 
-  render() {
-    return (
-      <section className="section auth">
-        <div className="container">
-          <h1 className="dogify">Dogify</h1>
-          <h3>Register</h3>
-          <FormErrors formerrors={this.props.errors} />
 
-          <form className="parent-form" onSubmit={this.handleSubmit}>
-            <div className="field">
-              <p className="control">
-                <input
-                  className="input"
-                  type="text"
-                  id="username"
-                  aria-describedby="userNameHelp"
-                  placeholder="Enter username"
-                  value={this.props.username}
-                  onChange={this.onInputUsernameChange}
-                />
-              </p>
-            </div>
-            <div className="field">
-              <p className="control has-icons-left has-icons-right">
-                <input
-                  className="input"
-                  type="email"
-                  id="email"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter email"
-                  value={this.props.email}
-                  onChange={this.onInputEmailChange}
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-envelope"></i>
-                </span>
-              </p>
-            </div>
-            <div className="field">
-              <p className="control has-icons-left">
-                <input
-                  className="input"
-                  type="password"
-                  id="password"
-                  placeholder="Password"
-                  value={this.props.password}
-                  onChange={this.onInputPasswordChange}
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-lock"></i>
-                </span>
-              </p>
-            </div>
-            <div className="field">
-              <p className="control has-icons-left">
-                <input
-                  className="input"
-                  type="password"
-                  id="confirmpassword"
-                  placeholder="Confirm password"
-                  value={this.props.confirmPassword}
-                  onChange={this.onInputConfirmPasswordChange}
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-lock"></i>
-                </span>
-              </p>
-            </div>
-            <div className="field">
-              <p className="control">
-                <button className="button is-success">
-                  Register
+  return (
+    <section className="section auth">
+      <div className="container">
+        <h1 className="dogify">Dogify</h1>
+        <h3>Register</h3>
+        <FormErrors formerrors={props.errors} />
+
+        <form className="parent-form" onSubmit={handleSubmit}>
+          <div className="field">
+            <p className="control">
+              <input
+                className="input"
+                type="text"
+                id="username"
+                aria-describedby="userNameHelp"
+                placeholder="Enter username"
+                value={props.username}
+                onChange={onInputUsernameChange}
+              />
+            </p>
+          </div>
+          <div className="field">
+            <p className="control has-icons-left has-icons-right">
+              <input
+                className="input"
+                type="email"
+                id="email"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+                value={props.email}
+                onChange={onInputEmailChange}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+            </p>
+          </div>
+          <div className="field">
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="password"
+                id="password"
+                placeholder="Password"
+                value={props.password}
+                onChange={onInputPasswordChange}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock"></i>
+              </span>
+            </p>
+          </div>
+          <div className="field">
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="password"
+                id="confirmpassword"
+                placeholder="Confirm password"
+                value={props.confirmPassword}
+                onChange={onInputConfirmPasswordChange}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock"></i>
+              </span>
+            </p>
+          </div>
+          <div className="field">
+            <p className="control">
+              <button className="button is-success">
+                Register
                 </button>
-              </p>
-            </div>
-          </form>
-        </div>
-      </section>
-    );
-  }
+            </p>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 const mapStateToProps = state => ({
